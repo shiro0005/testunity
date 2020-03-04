176d21b2cb368d0fb88b66d3629a0790bfd0b663
shiro0005 <siawase.youta@gmail.com>
2020-03-04 16:32:15 +0900

    あ

--- testunity/Assets/Scenes/AnatahaNekoGaSukidesuka.cs
+++ testunity/Assets/Scenes/AnatahaNekoGaSukidesuka.cs
@@ -7,7 +7,7 @@ public class AnatahaNekoGaSukidesuka : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("ネコが嫌いな人などいない");
+        
     }
 
     // Update is called once per frame
